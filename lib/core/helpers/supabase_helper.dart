377b3dbc9b28718e293aa81ef5b4ff11d3a30727
poojac-nt <poojacho.nt@gmail.com>
2025-12-01 12:54:57 +0530

    fix : student role error in registration
    - Fixed role in RLS policy to solve error while fetching current version

--- lib/core/helpers/supabase_helper.dart
+++ lib/core/helpers/supabase_helper.dart
@@ -114,7 +114,7 @@ class SupabaseHelper {
                 'full_name': data.name,
                 'address': data.address,
                 'number': data.number,
-                'role': 'student',
+                'role': 'Student',
                 'user_email': data.email,
                 'auth_id': userId,
                 'profile_picture': data.profilePicture,
